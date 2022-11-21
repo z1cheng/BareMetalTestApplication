edfb57f8e7ee9d908d3216cf9a786b8a13312ecb
Chen Chen <chenchen2@microsoft.com>
2022-11-21 14:35:01 +0800

    Initial Commit

---
+++ BareMetalTestApplication/Controllers/PingController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BareMetalTestApplication.Controllers
+{
+    [ApiController]
+    public class PingController : Controller
+    {
+
+        [HttpGet(template:"/api/ping")]
+        public IActionResult Ping()
+        {
+            return Ok("Pong");
+        }
+    }
+}
---
+++ BareMetalTestApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BareMetalTestApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
